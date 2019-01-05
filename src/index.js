a033f61a80ecbfc119bac10cc194e167b85e8983
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-04 20:33:54 -0700

    make button interactive

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css'
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('clicked')}>
         {this.props.value}
       </button>
     );
